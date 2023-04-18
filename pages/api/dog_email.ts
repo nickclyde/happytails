9d0a9198969d7ce3e91f0bd0eeb2334aed4f9fe1
Nick Clyde <nick@clyde.tech>
2023-04-18 09:48:54 -0700

    Add cors

--- pages/api/dog_email.ts
+++ pages/api/dog_email.ts
@@ -1,9 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import sendgrid from '@sendgrid/mail';
+import Cors from 'cors'
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '');
 
+const cors = Cors({
+  origin: ['https://happytails.org', 'https://www.happytails.org'],
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 async function reformatDogApp(req: NextApiRequest, res: NextApiResponse) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+  
   let response;
   console.log(req.body)
   // try {
