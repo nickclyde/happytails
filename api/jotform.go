ac3214f5dde305b3d861f0a5418b842771657006
Nick Clyde <nick@clyde.tech>
2024-11-14 21:36:28 -0800

    Write request to response

--- api/jotform.go
+++ api/jotform.go
@@ -2,9 +2,19 @@ package jotform
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func Jotform(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Happy Tails!</h1>")
+	// Copy the request body to the response
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	// Write the contents of the request body to the response
+	fmt.Fprintf(w, "Request body:\n\n%s", body)
 }
