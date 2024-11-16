a504a2e7d98ef5eca8a7883d97a75400a9708b74
Nick Clyde <nick@clyde.tech>
2024-11-16 13:53:12 -0800

    Revert "Write request to response"
    This reverts commit ac3214f5dde305b3d861f0a5418b842771657006.

--- api/jotform.go
+++ api/jotform.go
@@ -2,19 +2,9 @@ package jotform
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 )
 
 func Jotform(w http.ResponseWriter, r *http.Request) {
-	// Copy the request body to the response
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Unable to read request body", http.StatusInternalServerError)
-		return
-	}
-	defer r.Body.Close()
-
-	// Write the contents of the request body to the response
-	fmt.Fprintf(w, "Request body:\n\n%s", body)
+	fmt.Fprintf(w, "<h1>Hello from Happy Tails!</h1>")
 }
